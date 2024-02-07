a3ef5f4d0f8add47606b19775bd3c38dc18170dc
Matheus Carvalho <matheuso.c08@gmail.com>
2024-02-06 23:05:26 -0300

    Calling the new classes and attacks functions

--- Program.cs
+++ Program.cs
@@ -3,21 +3,21 @@
 using System.Runtime.InteropServices;
 using dio_project_net_poo_rpg.src.Entities;
 
-Hero hero = new Hero();
+Knight hero = new Knight();
 hero.Name = "Arus";
 hero.Level = 12;
 hero.HeroType = "Warrior";
 
 Wizard whiteWizard = new Wizard("Jenica", 15, "Mago branco");
 
+Knight opponent = new Knight("Malevicus", 59, "Devil");
+
 
 System.Console.WriteLine($"{hero}");
 
-Hero opponent = new Hero("Malevicus", 59, "Devil");
-
 System.Console.WriteLine(hero.Attack());
-System.Console.WriteLine();
+
 System.Console.WriteLine(whiteWizard);
 System.Console.WriteLine(whiteWizard.Attack());
-System.Console.WriteLine(whiteWizard.Attack(20));
+System.Console.WriteLine(whiteWizard.Attack(2));
 
