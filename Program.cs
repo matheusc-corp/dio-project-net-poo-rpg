1d5e84c2e00327259ec439aedf33f3eaebcab67f
Matheus Carvalho <matheuso.c08@gmail.com>
2024-02-06 21:00:35 -0300

    feat: Included an example for the hero

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string nome = "Arus";
+int level = 12;
+string tipoHeroi = "Guerreiro";
+
+System.Console.WriteLine($"Nome: {nome}\nLvl: {level}\nClasse: {tipoHeroi}");
