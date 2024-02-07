91b558b4b78cfc7571b004e7548e500ca45e41e1
Matheus Carvalho <matheuso.c08@gmail.com>
2024-02-06 21:01:29 -0300

    feat: Created a class for the hero character

---
+++ src/Entities/Arus.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dio_project_net_poo_rpg.src.Entities
+{
+    public class Arus
+    {
+        
+    }
+}
